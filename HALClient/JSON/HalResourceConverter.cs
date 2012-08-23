b19ded385eb3ac19e917a69764d2cdae2389e127
Niklas Närhinen <niklas.narhinen@ecom.fi>
2012-08-23 14:07:38 +0300

    Cleanup HalResourceConverter

--- HALClient/JSON/HalResourceConverter.cs
+++ HALClient/JSON/HalResourceConverter.cs
@@ -19,47 +19,30 @@ namespace Ecom.Hal.JSON
 		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
 		{
 			var obj = JToken.ReadFrom(reader);
-			if (obj.Type == JTokenType.Object) {
-				var ret = JsonConvert.DeserializeObject(obj.ToString(), objectType, new JsonConverter[] { });
-				if (obj["_embedded"] != null && obj["_embedded"].HasValues) {
-					var enumerator = ((JObject) obj["_embedded"]).GetEnumerator();
-					while (enumerator.MoveNext()) {
-						var rel = enumerator.Current.Key;
-						foreach (var property in objectType.GetProperties()) {
-							var attribute = property.GetCustomAttributes(true).FirstOrDefault(attr => attr is HalEmbeddedAttribute &&
-																																												((HalEmbeddedAttribute) attr).Rel == rel);
-							if (attribute != null) {
-								property.SetValue(ret,
-																	JsonConvert.DeserializeObject(enumerator.Current.Value.ToString(), property.PropertyType,
-																																new JsonConverter[] {new HalResourceConverter()}), null);
-							}
+			var ret = JsonConvert.DeserializeObject(obj.ToString(), objectType, new JsonConverter[] { });
+			if (obj["_embedded"] != null && obj["_embedded"].HasValues) {
+				var enumerator = ((JObject) obj["_embedded"]).GetEnumerator();
+				while (enumerator.MoveNext()) {
+					var rel = enumerator.Current.Key;
+					foreach (var property in objectType.GetProperties()) {
+						var attribute = property.GetCustomAttributes(true).FirstOrDefault(attr => attr is HalEmbeddedAttribute &&
+																																											((HalEmbeddedAttribute) attr).Rel == rel);
+						if (attribute != null) {
+							property.SetValue(ret,
+																JsonConvert.DeserializeObject(enumerator.Current.Value.ToString(), property.PropertyType,
+																															new JsonConverter[] {new HalResourceConverter()}), null);
 						}
 					}
 				}
-				if (objectType.IsSubclassOf(typeof(HalResource)))
-					((HalResource) ret).IsNew = false;
-				return ret;
 			}
-			if (obj.Type == JTokenType.Array) {
-				IEnumerable ret;
-				if (objectType == typeof(IEnumerable<>)) {
-					ret = Activator.CreateInstance(typeof(List<>).MakeGenericType(objectType.GetGenericArguments())) as IEnumerable;
-				}
-				else {
-					ret = objectType.GetConstructor(new Type[] {}).Invoke(new object[] {}) as IEnumerable;
-				}
-				foreach (var jObject in ((JArray) obj)) {
-					((IList) ret).Add(JsonConvert.DeserializeObject(jObject.ToString(), objectType.GetGenericArguments().FirstOrDefault(),
-					                                                new JsonConverter[] {new HalResourceConverter()}));
-				}
-				return ret;
-			}
-			return null;
+			if (objectType.IsSubclassOf(typeof(HalResource)))
+				((HalResource) ret).IsNew = false;
+			return ret;
 		}
 
 		public override bool CanConvert(Type objectType)
 		{
-			return true;
+			return objectType.IsSubclassOf(typeof(HalResource));
 		}
 	}
 }
